4ce8661fb49ed877ee56b19fd96c39af27881376
Artur <artur071982@gmail.com>
2021-03-07 21:07:21 +0500

    load

---
+++ HeloWorldToBack/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HeloWorldToBack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word= "Hello, world!";
+            for (int i = word.Length-1; i > -1; i--)//цикл с конца строкм
+            {
+                Console.WriteLine(word[i]); // посимвольный вывод .
+            }
+
+
+        }
+    }
+}
